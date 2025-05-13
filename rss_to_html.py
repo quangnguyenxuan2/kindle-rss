a1eed3fa2697e2ad59239cd09cc0d0472322e260
QUANG Nguyen Xuan <quangnguyenxuan2@gmail.com>
2025-05-13 18:30:52 +0700

    Update rss_to_html.py

--- rss_to_html.py
+++ rss_to_html.py
@@ -9,6 +9,6 @@ os.makedirs("public", exist_ok=True)
 with open("public/index.html", "w", encoding="utf-8") as f:
     f.write("<html><head><meta charset='UTF-8'><title>Báo Mới</title></head><body>")
     f.write("<h1>Tin mới từ VnExpress</h1><ul>")
-    for entry in feed.entries[:10]:
+    for entry in feed.entries[:30]:
         f.write(f"<li><a href='{entry.link}'>{entry.title}</a></li>")
     f.write("</ul></body></html>")
